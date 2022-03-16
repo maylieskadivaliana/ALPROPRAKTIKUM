56566d230244ec7bfd91a2289dec59f1db2e6e3d
maylieska divaliana <maymaylies@gmail.com>
2022-03-16 15:41:15 +0700

    program kalkulator

---
+++ [2100018188][2100018185][2100018212].cpp
@@ -0,0 +1,18 @@
+/*NAMA		: MAYLIESKA DIVALIANA SETIAWAN
+KELAS		: D
+NIM		: 2100018188
+NAMA		: ADITYA ZAHRAN MAHESWARA SUDRAJAT
+KELAS		: D
+NIM		: 2100018212
+NAMA		: ADNAN FAUZAN USMAN
+KELAS		: D
+NIM		: 2100018185
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+cout << "hello world";
+return 0;
+}
+
