b818de0f0bb598faee2ef85dd59f9ac079f1d96d
Aditya-Zahran-MS <101700058+Aditya-Zahran-MS@users.noreply.github.com>
2022-03-16 16:08:52 +0700

    Update [2100018188][2100018185][2100018212].cpp

--- [2100018188][2100018185][2100018212].cpp
+++ [2100018188][2100018185][2100018212].cpp
@@ -44,5 +44,33 @@ int main()
 	float c,d;
 	cout<<"SELAMAT DATANG DI PROGRAM KALKULATOR SEDERHANA"<<endl<<endl<<endl;
 }
+
+int main()
+{
+	int pilihan, n, hasil,i ;
+	float total ; 
+	int a,b ;
+	float c,d;
+	cout<<"SELAMAT DATANG DI PROGRAM KALKULATOR SEDERHANA"<<endl<<endl<<endl ;
+	cout<<"||====================================================||"<<endl ;
+	cout<<"||     SILAHKAN PILIH PERINTAH YANG ANDA INGINKAN     ||"<<endl ;
+	cout<<"||                                                    ||"<<endl ;
+	cout<<"||  1. Pertambahan ( + )                              ||"<<endl ;
+	cout<<"||  2. Pengurangan ( - )                              ||"<<endl ;
+	cout<<"||  3. Perkalian   ( x )                              ||"<<endl ;
+	cout<<"||  4. Pembagian   ( : )                              ||"<<endl ;
+	cout<<"||                                                    ||"<<endl ;
+	cout<<"||====================================================||"<<endl<<endl<<endl ;
+	cout<<"MASUKAN PERINTAH YANG ANDA INGINKAN = " ;
+	cin>>pilihan ;
+	cout<<endl<<endl ;
 	
+	if(pilihan == 1)
+	{ 
+		cout<<"Masukan nilai A = ";
+		cin>>a;
+		cout<<"Masukan nilai B = ";
+		cin>>b;
+		tambah(a,b);
+	}	
 
